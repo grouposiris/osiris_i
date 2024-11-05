28ef3d34d72d98114af758337330fde26b32097d
rafaelnmo <rafaelnevesmello@gmail.com>
2024-11-05 11:28:36 -0300

    Removing comments to make RTL clear to read

--- verilog/rtl/osiris_i/core.v
+++ verilog/rtl/osiris_i/core.v
@@ -105,54 +105,7 @@ module core #(
         .o_addr_src_ID  (o_addr_src_ID),    // Address source for ID stage
         .o_fence_ID     (o_fence_ID)        // Fence signal for ID stage
     );
-    // // opcode for load instructions
-    // localparam LOAD_OP = 7'b0000011;
 
-    // // Define the load type based on o_op and o_funct3
-    // reg [1:0] load_type;
-    // always @(*) begin
-    //     if (o_op == LOAD_OP) begin
-    //         case (o_funct3)
-    //             3'b000:  load_type = 2'b00;  // LB: Load Byte
-    //             3'b001:  load_type = 2'b01;  // LH: Load Halfword
-    //             3'b010:  load_type = 2'b01;  // LH: Load Halfword
-    //             // Add more cases for other load instructions if needed (e.g., LBU, LHU)
-    //             default: load_type = 2'b00;  // Default to LB if not recognized
-    //         endcase
-    //     end else begin
-    //         load_type = 2'b00;  // Default value if not a load operation
-    //     end
-    // end
-
-    // // Calculate the effective address
-    // wire [31:0] effective_addr = o_data_addr_M;
-
-    // // Load the data based on the load type
-    // always @(*) begin
-    //     case (load_type)
-    //         2'b00: begin  // LB: Load Byte
-    //             // Extract the byte based on the least significant bits of the address
-    //             case (effective_addr[1:0])
-    //                 2'b00: loaded_data = {{24{i_read_data_M[7]}}, i_read_data_M[7:0]};  // Byte 0
-    //                 2'b01: loaded_data = {{24{i_read_data_M[15]}}, i_read_data_M[15:8]};  // Byte 1
-    //                 2'b10: loaded_data = {{24{i_read_data_M[23]}}, i_read_data_M[23:16]};  // Byte 2
-    //                 2'b11: loaded_data = {{24{i_read_data_M[31]}}, i_read_data_M[31:24]};  // Byte 3
-    //                 default: loaded_data = 32'b0;
-    //             endcase
-    //         end
-    //         2'b01: begin  // LH: Load Halfword
-    //             // Extract the halfword based on the least significant bit of the address
-    //             case (effective_addr[1])
-    //                 1'b0:
-    //                 loaded_data = {{16{i_read_data_M[15]}}, i_read_data_M[15:0]};  // Halfword 0
-    //                 1'b1:
-    //                 loaded_data = {{16{i_read_data_M[31]}}, i_read_data_M[31:16]};  // Halfword 1
-    //                 default: loaded_data = 32'b0;
-    //             endcase
-    //         end
-    //         default: loaded_data = 32'b0;  // Default case
-    //     endcase
-    // end
 
     // ------------------------------------------
     // Instantiate Datapath
